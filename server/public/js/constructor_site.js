ae89e429aa0de079dbf6e4f89c5314f8e2d606d0
Lilya Kostiv <lilya.kos@gmail.com>
2021-02-09 21:14:55 +0200

    Version 9 (final) : find,  design for header

--- server/public/js/constructor_site.js
+++ server/public/js/constructor_site.js
@@ -3,41 +3,49 @@
         main : {
             url: "/main.html",
             class: "container",
-            functionjs: function () {main();}
+            functionjs: function () {main();},
+            activePage: 1
         },
         plan : {
             url: "/plan.html",
             class: "container",
-            functionjs: function () {plan();}
+            functionjs: function () {plan();},
+            activePage: 2
         },
         marks : {
             url: "/marks.html",
             class: "container",
-            functionjs: function () {marks();}
+            functionjs: function () {marks();},
+            activePage: 2
         },
         travel : {
             url: "/travel.html",
             class: "container carousel-size",
-            functionjs: function () {travel();}
+            functionjs: function () {travel();},
+            activePage: 2
         },
         travel_future : {
             url: "/travel_future.html",
             class: "container carousel-size",
-            functionjs: function () {travel_future();}
+            functionjs: function () {travel_future();},
+            activePage: 2
         },
         control : {
             url: "/control.html",
             class: "container",
-            functionjs: function () {control();}
+            functionjs: function () {control();},
+            activePage: 3
         },
         form_test : {
             url: "/form_test.html",
             class: "container",
-            functionjs: function () {form_test();}
+            functionjs: function () {form_test();},
+            activePage: 3
         },
         homework : {
             url: "/homework.html",
-            class: "container"
+            class: "container",
+            activePage: 3
         }   
     };
 
@@ -56,6 +64,31 @@
             getPageSite.then(data => data.text() //data.text() - теж проміс
                             .then(function(text){loadPage(text,mainArea, selectedPageSite.functionjs);}))
                         .catch(error=>console.log(error));
+            
+            let linkMain = document.getElementById("main");
+            let parentlinkMain = linkMain.parentElement;
+            let linkPlan = document.getElementById("plan");
+            let parentlinkPlan = linkPlan.parentElement.parentElement;
+            let linkHomework = document.getElementById("homework");
+            let parentlinkHomework = linkHomework.parentElement.parentElement;
+            switch(selectedPageSite.activePage){
+                case 1: 
+                    parentlinkPlan.classList.remove("active");
+                    parentlinkHomework.classList.remove("active");
+                    parentlinkMain.classList.add("active");
+                    break
+                case 2: 
+                    parentlinkHomework.classList.remove("active");
+                    parentlinkMain.classList.remove("active");
+                    parentlinkPlan.classList.add("active");
+                    break
+                case 3: 
+                    parentlinkMain.classList.remove("active");
+                    parentlinkPlan.classList.remove("active");
+                    parentlinkHomework.classList.add("active");
+                    break
+            }
+            
         }
         
     });
@@ -69,6 +102,52 @@
 
     document.getElementById("main").click();
 
+    const findInfo = {
+        в :["1вектор","4відень","1вирази"],
+        м: ["1масив","1матриця"],
+        о: ["3оцінки"],
+        к: ["6контрольна"],
+        п: ["2план"],
+        ф: ["1функції","файли"],
+        w: ["1word"]
+    }
+    const numberPage = {
+        1 : "Головна",
+        2: "Планування тем",
+        3: "Оцінки",
+        4: "Подорожі",
+        5: "Майбутні подорожі",
+        6: "Контрольна робота",
+        7: "Тестування",
+        8: "Домашнє завдання"
+    }
+    let btnSearch = document.querySelector("#btn-search");
+    btnSearch.addEventListener('click', (event) => { 
+        let mainArea = document.querySelector("#area-search"); 
+        let found;
+        let value;
+        let stringWords;
+        for (let key in findInfo) {      
+            if (key==mainArea.value[0]) {
+                stringWords="";
+                for (value of findInfo[key]){
+                    let value2= value.substring(1);
+                    stringWords=stringWords+`${value2}`+", ";
+                    if (value2==mainArea.value) 
+                        {
+                          found = value2;
+                          break;
+                        }
+                }
+            }   
+        }
+        if (found) {
+            alert(`Інформація про ${mainArea.value} на сторінці "${numberPage[value[0]]}" `);      
+        } else {if (stringWords!==undefined) alert(`Інформації про ${mainArea.value} немає на сайті.  
+Можливо Вас зацікавить інформація про ${stringWords}`); 
+                else alert(`Інформації про ${mainArea.value} немає на сайті !`);   
+                } 
+    })
     }) ()
     
 
